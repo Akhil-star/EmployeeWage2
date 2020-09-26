b7553d34ebc8edee641c202470eb63aae8ba8f6e
Akhil-star <akhilakki1305@gmail.com>
2020-09-26 15:21:51 +0530

    Monthly Wage

--- src/main/java/com/employeewage2/App.java
+++ src/main/java/com/employeewage2/App.java
@@ -9,13 +9,15 @@ public class App
 	public static final int IS_FULL_TIME=2;
 	public static final int IS_PART_TIME=1;
 	public static final int EMP_RATE_HOUR=20;
-	
+	public static final int NUM_DAYS=20;
 
     public static void main( String[] args ) {
-    
+    	
     	int empHrs=0;
     	int empWage=0;
+    	int totalWage=0;
     	
+    	for(int day=0; day<NUM_DAYS; day++) {
     	int empCheck =(int) Math.floor(Math.random() *10)%3;
     	switch(empCheck) {
         case IS_FULL_TIME:
@@ -27,9 +29,12 @@ public class App
     	default :
     		empHrs=0;
     		break;
-    }
+       }
     	empWage=empHrs*EMP_RATE_HOUR;
+    	totalWage += empWage;
     	System.out.println("Emp Wage: " +empWage);
-   
-   }
+    } 
+	System.out.println("Total Emp Wage: " +totalWage);
+
+  }
 }
