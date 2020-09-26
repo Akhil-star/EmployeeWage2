446f0ef2a294d5ab07520040b71b69594c0ea522
Akhil-star <akhilakki1305@gmail.com>
2020-09-26 15:03:14 +0530

    Employee present/absent

--- src/main/java/com/employeewage2/App.java
+++ src/main/java/com/employeewage2/App.java
@@ -5,9 +5,17 @@ package com.employeewage2;
  *
  */
 public class App 
-{
+{	
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to employee wage computation" );
+        int IS_FULL_TIME=1;
+        double empCheck = Math.floor(Math.random() * 10)%2;
+		
+    			
+    	if(empCheck == IS_FULL_TIME) {
+    	System.out.println("Employee present");
+    	} else {
+    	System.out.println("Employee absent");
+    	}
     }
 }
